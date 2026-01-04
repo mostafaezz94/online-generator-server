1ebc69b2a91399930116f11db91a5897dae02e52
mostafaezz94 <156386401+mostafaezz94@users.noreply.github.com>
2026-01-05 01:49:15 +0300

    Set up Express server with basic routes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/check", (req, res) => {
+  res.json({
+    status: "ok",
+    time: Date.now()
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
